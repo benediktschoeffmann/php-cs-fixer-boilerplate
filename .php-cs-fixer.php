262122200cc48c54e73a7dae09babe3ef6819645
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2023-04-14 16:24:19 +0200

    Initial commit

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude([
+        '.build/',
+        '.docker/',
+        '.github/',
+    ])
+    ->ignoreDotFiles(false)
+    ->in(__DIR__)
+    ->name('.php-cs-fixer.php');
+
+$config = new PhpCsFixer\Config();
+
+$config
+    ->setFinder($finder);
+    ->setRules([]);
+
+return $config;
